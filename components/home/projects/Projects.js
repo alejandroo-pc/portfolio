3574e70b5e1f1b1146a540c6730ace6215bdbdba
Alejandro Patino Camargo <alejandro@Alejandros-MacBook-Pro.local>
2023-06-11 12:43:42 -0700

    Updated experience cards. Updated Projects section, added new project and updated images

--- components/home/projects/Projects.js
+++ components/home/projects/Projects.js
@@ -18,6 +18,42 @@ export const Projects = () => {
 
 const projects = [
   {
+    title: "PHRAZ_L",
+    imgSrc: "project-imgs/practicum.png",
+    code: "https://github.com/Code-the-Dream-School/bb-practicum-team-2-back",
+    projectLink: "https://team2-front.vercel.app/",
+    tech: ["React", "Tailwind", "Socket-io", "NodeJS", "Express"],
+    description:
+      "A wordl type game that challenges players to guess a word or phrase.",
+    modalContent: (
+      <>
+        <p>
+          Our game, built using NodeJS, Express, and React. Our tech stack
+          allowed us to build a robust game that is both easy to use and
+          visually stunning.
+        </p>
+        <p>
+          During the 8 week project, we followed an agile workflow, planning
+          sprints and iterating on features. This allowed us to quickly adapt to
+          changes and improve the game as we went along.
+        </p>
+        <p>
+          NodeJS allowed us to build a highly responsive game server that can
+          handle a large number of players simultaneously. We used Express to
+          build the API layer, which enabled us to create secure and reliable
+          endpoints for game actions and data retrieval.
+        </p>
+        <p>
+          As part of our development process, we used Git for version control,
+          pull requests, and conflict merging. This gave us the ability to
+          collaborate effectively as a software development team and maintain a
+          high level of code quality throughout the project.
+        </p>
+      </>
+    ),
+  },
+
+  {
     title: "Song Request API",
     imgSrc: "project-imgs/song-request.png",
     code: "https://github.com/Alejandro-Patino-Camargo/song-request-api",
@@ -28,48 +64,35 @@ const projects = [
     modalContent: (
       <>
         <p>
-          A REST API for a song request app. The API is built with NodeJS, Express, and MongoDB.It is deployed on Render.
+          A REST API for a song request app. The API is built with NodeJS,
+          Express, and MongoDB and is deployed on Render.
         </p>
         <p>
-          The app is built with a node backend, with data stored in MongoDB.
-          The frontend is built vanilla html and css. The API is deployed on Render.
+          The app is built with a node backend, with data stored in MongoDB. The
+          frontend is built vanilla html and css. The API is deployed on Render.
         </p>
         <p>
-          Since this is my first app, I had an emphisis in building a strong backend.
-          The frontend is intended to be minimal and easily navigated.{" "}
+          Since this is my first app, I had an emphisis in building a strong
+          backend. The frontend is intended to be minimal and easily navigated.{" "}
         </p>
       </>
     ),
   },
+
   {
-    title: "PHRAZ_L",
-    imgSrc: "project-imgs/practicum.png",
-    code: "https://github.com/Code-the-Dream-School/bb-practicum-team-2-back",
-    projectLink: "https://team2-front.vercel.app/",
-    tech: ["React", "Tailwind", "Socket-io", "NodeJS", "Express"],
+    title: "Linear Algebra Matrix Determinant Calculator",
+    imgSrc: "project-imgs/matrix.png",
+    code: "https://github.com/Alejandro-Patino-Camargo/Determinant-Calculator",
+    projectLink:
+      "https://replit.com/@AlejandroPatinoCamargo/Matrix-Determinant-calculator#main.cpp",
+    tech: ["C++"],
     description:
-      "A wordl type game that challenges players to guess a word or phrase (In development).",
+      "The following C++ code will calculate the determinant of a 2x2 - 10x10 matrix",
     modalContent: (
       <>
         <p>
-          Our game, built using NodeJS, Express, and React.
-          Our tech stack allowed us to build a robust game that is both easy to use and visually stunning.
-        </p>
-        <p>
-          NodeJS allowed us to build a highly responsive game server that can handle a large number of players simultaneously.
-          We used Express to build the API layer, which enabled us to create secure and reliable endpoints for game actions and data retrieval.
-        </p>
-        <p>
-          React was used to create the game's user interface, providing a seamless and intuitive experience for players.
-          Its modular and reusable components helped us to create a visually stunning game that is easy to navigate and interact with.
-        </p>
-        <p>
-          During the 8 week project, we followed an agile workflow, planning sprints and iterating on features.
-          This allowed us to quickly adapt to changes and improve the game as we went along.
-        </p>
-        <p>
-          As part of our development process, we also used Git for version control, pull requests, and conflict merging.
-          This gave us the ability to collaborate effectively as a software development team and maintain a high level of code quality throughout the project.
+          This is my final project for linear algebra. The following C++ code
+          will calculate the determinant of a 2x2 - 10x10 matrix.
         </p>
       </>
     ),
