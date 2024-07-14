4eaa383931dae9978f2aa88662c89d1cb1966ed1
alejandro-pc <alejandropatinoc@gmail.com>
2024-07-13 20:53:00 -0700

    fix: removed broken about section

--- components/nav/SideBar.js
+++ components/nav/SideBar.js
@@ -38,18 +38,6 @@ export const SideBar = () => {
       <motion.a
         initial={{ x: -70 }}
         animate={{ x: 0 }}
-        transition={{ duration: 0.5, delay: 0.1 }}
-        href='#about'
-        onClick={() => {
-          setSelected('about');
-        }}
-        className={selected === 'about' ? styles.selected : ''}
-      >
-        About
-      </motion.a>
-      <motion.a
-        initial={{ x: -70 }}
-        animate={{ x: 0 }}
         transition={{ duration: 0.5, delay: 0.2 }}
         href='#projects'
         onClick={() => setSelected('projects')}
