4d5db9328ccb1ed9406f5b784066224236bc71dc
alejandro-pc <alejandropatinoc@gmail.com>
2024-12-03 22:06:48 -0800

    updated names and role

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -1,12 +1,12 @@
 import { InlineCode } from "@/once-ui/components";
 
 const person = {
-  firstName: "Selene",
-  lastName: "Yu",
+  firstName: "Alejandro",
+  lastName: "Patino C.",
   get name() {
     return `${this.firstName} ${this.lastName}`;
   },
-  role: "Design Engineer",
+  role: "Software Engineer",
   avatar: "/images/avatar.jpg",
   location: "America/Los_Angeles", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
   languages: [], // optional: Leave the array empty if you don't want to display languages
@@ -289,4 +289,3 @@ const gallery = {
 };
 
 export { person, social, newsletter, home, about, blog, work, gallery };
-
