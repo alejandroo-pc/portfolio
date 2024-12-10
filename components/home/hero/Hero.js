36e0bdd521626dccd23269b8e6bab5f523c998b9
alejandro-pc <alejandropatinoc@gmail.com>
2024-12-09 21:36:00 -0800

    fixed title typo, removed stack

--- components/home/hero/Hero.js
+++ components/home/hero/Hero.js
@@ -13,7 +13,7 @@ export const Hero = () => {
         </Reveal>
         <Reveal>
           <h2 className={styles.subTitle}>
-            <span>Software Stack Engineer</span>
+            <span>Software Engineer</span>
           </h2>
         </Reveal>
         <Reveal>
